774e462dda39be99af28a327ea41fdcc252e9744
yuji masujima <masjima2712@gmail.com>
2020-05-03 18:16:41 +0900

    Update template for responsib

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(function(){
-  console.log("OK?")
+  console.log("OK?");
+
+  $(".hamburgerMenu").click(function(){
+    var hideMenu = $(".hideMenu");
+    var displayNone = "d-none";
+    if(hideMenu.hasClass(displayNone)){
+      hideMenu.removeClass(displayNone);
+    } else {
+      hideMenu.addClass(displayNone);
+    }
+  })
+  
 })
