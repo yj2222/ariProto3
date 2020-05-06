e0b49e8a500db15d883d9d444d735203461b5277
yuji masujima <masjima2712@gmail.com>
2020-05-06 11:29:44 +0900

    Update template

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-  console.log("OK?");
 
   $(".hamburgerMenu").click(function(){
     var hideMenu = $(".hideMenu");
@@ -11,4 +10,18 @@ $(function(){
     }
   })
   
+  $("#fonmConfirmBtn").click(function(e){
+    var contactForm__name = $("#contactForm__name").val()
+    var contactForm__email = $("#contactForm__email").val()
+    var contactForm__message = $("[name=contactForm__message]").val().replace(/\r|\n|\r\n/g, '<br>')
+    $(".formConfirmModal").removeClass("d-none");
+    $("#formConfirm__name").text(contactForm__name);
+    $("#formConfirm__email").text(contactForm__email);
+    $("#formConfirm__message").html (contactForm__message);
+  })
+
+  $(".formConfirmModal, #formConfirmModal__close").click(function(){
+    $(this).addClass("d-none");
+  })
+
 })
